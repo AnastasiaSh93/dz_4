9a853de703db4e9fce39468a42a6cb65e27369f3
AnasyasiaSh93 <nargizaflower047@gmail.com>
2023-06-19 19:56:08 +0200

    Added classes: Main and App

---
+++ dz_4/src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+    int int_field = 3;
+    public int getInt()
+    {
+        System.out.println("---"+ int_field + "---");
+        return int_field;
+    }
+
+
+}
---
+++ dz_4/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        App new_getInt = new App();
+        System.out.println("~~~" + new_getInt.getInt() + "~~~");
+    }
+}
